298fe80431bafcebc0e0e23fe52590119a5e6902
segmentationfaulter <saqib@qubit.com>
2018-02-13 11:41:24 +0500

    sequence

---
+++ b/hw4.rkt
@@ -0,0 +1,9 @@
+
+#lang racket
+
+(provide (all-defined-out)) ;; so we can put tests in a second file
+
+(define (sequence low high stride)
+  (if (> low high)
+      null
+      (cons low (sequence (+ low stride) high stride))))
